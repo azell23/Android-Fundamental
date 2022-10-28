db066dcc524c5fef80c07cb17142a8ed0b258865
Hildan Rizky <31343574+azell23@users.noreply.github.com>
2022-10-28 11:22:38 +0700

    Update API service.kt

--- MyGitHubUser_2/app/src/main/java/com/example/mygithubuser/API/API service.kt
+++ MyGitHubUser_2/app/src/main/java/com/example/mygithubuser/API/API service.kt
@@ -8,27 +8,27 @@ import retrofit2.http.*
 interface APIservice {
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token (YOURTOKEN)")
     fun searchUser(
         @Query("q")
         query: String
     ): Call<SearchResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token (YOURTOKEN)")
     fun detailUser(
         @Path("username")
         username: String
     ): Call<UserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token (YOURTOKEN)")
     fun followersUser(
         @Path("username")
         username: String
     ): Call<ArrayList<UserResponse>>
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token (YOURTOKEN)")
     fun followingUser(
         @Path("username")
         username: String
